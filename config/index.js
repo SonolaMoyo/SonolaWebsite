017c7b5c7f53334081a8bc82e687a41b7515992b
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-08-17 17:01:52 +0100

    emailjs config

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = ""
+export const EMAILJS_TEMPLATE_ID = "template_h6uv819"
 
-export const EMAILJS_SERVICE_ID = ""
+export const EMAILJS_SERVICE_ID = "service_2r9zydo"
 
-export const EMAILJS_PUBLIC_KEY = ""
+export const EMAILJS_PUBLIC_KEY = "AdXpuz7dlx3KckYXJ"
